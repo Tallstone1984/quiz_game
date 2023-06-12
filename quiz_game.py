c3ea2eb3be75ebc5f031eb99cb3641d86b452465
Thomas Westerfield <97644848+Tallstone1984@users.noreply.github.com>
2023-06-12 16:32:39 -0500

    Update quiz_game.py
    Added amount of questions correct and added percentage

--- quiz_game.py
+++ quiz_game.py
@@ -6,28 +6,35 @@ if playing.lower() != "yes":
     quit()  
     
 print("Okay! Let's play :)")
-
+score = 0
 
 answer = input("wha does cpu stand for? ")
-if answer.lower == "central processing unit":
-    print("Correct!")   
+if answer.lower() == "central processing unit":
+    print('Correct!')  
+    score += 1 
 else: 
     print("Incorrect!")
     
 answer = input("what does GPU stand for? ")
 if answer.lower() == "graphics processing unit":
-    print("Correct!")   
+    print('Correct!')
+    score += 1   
 else: 
     print("Incorrect!")
     
 answer = input("what does RAM stand for? ")
 if answer.lower() == "random access memory":
-    print("Correct!")   
+    print('Correct!')
+    score += 1   
 else: 
     print("Incorrect!")
     
 answer = input("what does PSU stand for? ")
 if answer.lower() == "power supply unit":
-    print("Correct!")   
+    print('Correct!') 
+    score += 1  
 else: 
     print("Incorrect!")
+    
+print("You got " + str(score) + " questions correct!")
+print("You got " + str((score / 4) * 100) + "%.")
